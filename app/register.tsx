480eefa5120880528fbcec6d39066c70d46055ab
eiken <117394582+eikqn@users.noreply.github.com>
2026-09-20 09:55:42 -0300

    feat(component): add BackHeader component
    - register screen header replaced

--- app/register.tsx
+++ app/register.tsx
@@ -1,44 +1,7 @@
-import { View, Text, StyleSheet, Pressable } from "react-native"
-import { router } from "expo-router"
-import { colors } from "../constants/colors"
+import BackHeader from "../components/BackHeader"
 
 export default function Register() {
     return (
-        <View style={styles.container}>
-            <View style={styles.header}>
-                <Pressable style={styles.back} onPress={() => {router.back()}}>
-                    <Text style={styles.backIcon}>←</Text>
-                </Pressable>
-                <Text>Tela de cadastro</Text>
-            </View>
-        </View>
+        <BackHeader text="Cadastro de Perfil"></BackHeader>
     )
 }
-
-const styles = StyleSheet.create({
-    container: {
-        alignItems: "center",
-        flex: 1,
-        paddingTop: 50,
-    },
-
-    header: {
-        width: "90%",
-    },
-
-    back: {
-        alignItems: "center",
-        justifyContent: "center",
-        backgroundColor: colors.white2,
-        borderColor: colors.gray1,
-        borderRadius: "100%",
-        borderWidth: 2,
-        height: 48,
-        width: 48,
-    },
-
-    backIcon: {
-        fontSize: 24,
-        fontWeight: "bold",
-    },
-})
---
+++ components/BackHeader.tsx
@@ -0,0 +1,60 @@
+import { View, Pressable, Text, StyleSheet } from "react-native"
+import { router } from "expo-router"
+import { colors } from "../constants/colors"
+
+type BackHeaderProps = {
+    text?: string,
+    onPress?: () => void,
+}
+
+export default function BackHeader({
+    text,
+    onPress = () => {router.back()},
+}: BackHeaderProps) {
+    return (
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Pressable style={styles.back} onPress={onPress}>
+                    <Text style={styles.backIcon}>←</Text>
+                </Pressable>
+                <Text style={styles.text}>{text}</Text>
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        alignItems: "center",
+        flex: 1,
+        paddingTop: 50,
+    },
+
+    header: {
+        alignItems: "center",
+        flexDirection: "row",
+        gap: 12,
+        width: "90%",
+    },
+
+    back: {
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: colors.white2,
+        borderColor: colors.gray1,
+        borderRadius: "100%",
+        borderWidth: 2,
+        height: 48,
+        width: 48,
+    },
+
+    backIcon: {
+        fontSize: 24,
+        fontWeight: "bold",
+    },
+
+    text: {
+        fontSize: 20,
+        fontWeight: "bold",
+    },
+})
