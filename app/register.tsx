6261ecd9c2fb04889be8b37424a3cd2483cf1b3f
eiken <117394582+eikqn@users.noreply.github.com>
2026-09-23 17:35:56 -0300

    feat: rename SelectionCard component to ProfileCard

--- app/register.tsx
+++ app/register.tsx
@@ -1,8 +1,9 @@
 import { useState } from "react"
 import { View, StyleSheet } from "react-native"
+import { router } from "expo-router"
 import BackHeader from "../components/BackHeader"
 import SectionDescription from "../components/SectionDescription"
-import SelectionCard from "../components/SelectionCard"
+import ProfileCard from "../components/ProfileCard"
 import AppButton from "../components/AppButton"
 
 export default function Register() {
@@ -14,24 +15,24 @@ export default function Register() {
             <View style={styles.content}>
                 <SectionDescription title="Como deseja utilizar a plataforma?" description="Escolha a opção que melhor descreve você para iniciarmos."></SectionDescription>
                 <View style={styles.cards}>
-                    <SelectionCard 
+                    <ProfileCard 
                         title="Ser paciente" 
                         description="Desejo encontrar profissionais para cuidar da minha saúde em casa." 
                         selected={selectedCard === "patient"} 
                         onPress={() => setSelectedCard("patient")}
-                    ></SelectionCard>
-                    <SelectionCard 
+                    ></ProfileCard>
+                    <ProfileCard 
                         title="Sou Familiar/Responsável" 
                         description="Quero contratar e acompanhar atendimentos para um familiar de minha responsabilidade." 
                         selected={selectedCard === "guardian"} 
                         onPress={() => setSelectedCard("guardian")}
-                    ></SelectionCard>
-                    <SelectionCard 
+                    ></ProfileCard>
+                    <ProfileCard 
                         title="Sou Especialista da Saúde" 
                         description="Sou enfermeiro, fisio ou médico e quero oferecer meus serviços domiciliares." 
                         selected={selectedCard === "specialist"} 
                         onPress={() => setSelectedCard("specialist")}
-                    ></SelectionCard>
+                    ></ProfileCard>
                 </View>
                 <View style={styles.footer}>
                     <AppButton text="Continuar Cadastro"></AppButton>
