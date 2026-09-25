cd742d1b49be4415372c5753a028fa06920d0d42
eiken <117394582+eikqn@users.noreply.github.com>
2026-09-25 16:21:54 -0300

    feat: add address section to the patient register screen

--- app/patientRegister.tsx
+++ app/patientRegister.tsx
@@ -13,20 +13,21 @@ export default function patientRegister() {
                     title="Cadastro do Paciente" 
                     showDescription={true}
                     description="Adicione as informações do paciente que receberá os atendimentos."
+                    titleStyle={{fontSize: 22}}
                 ></SectionText>
                 <AppInput 
                     labelText="Nome Completo" 
-                    placeholder="Maria das Dores Oliveira"
+                    placeholder="Seu nome completo"
                 ></AppInput>
                 <View style={styles.row}>
                     <AppInput 
                         labelText="CPF" 
-                        placeholder="123.456.789-00" 
+                        placeholder="000.000.000-00" 
                         containerStyle={{flex: 1}}
                     ></AppInput>
                     <AppInput 
                         labelText="Data de Nascimento" 
-                        placeholder="12/03/1987" 
+                        placeholder="DD/MM/AAAA" 
                         containerStyle={{flex: 1}}
                     ></AppInput>
                 </View>
@@ -34,15 +35,46 @@ export default function patientRegister() {
                 <View style={styles.row}>
                     <AppInput 
                         labelText="E-mail" 
-                        placeholder="paciente@email.com" 
+                        placeholder="email@exemplo.com" 
                         containerStyle={{flex: 1}}
                     ></AppInput>
                     <AppInput 
                         labelText="Celular" 
-                        placeholder="(11) 99999-9999" 
+                        placeholder="(XX) XXXXX-XXXX" 
                         containerStyle={{flex: 1}}
                     ></AppInput>
                 </View>
+
+                {/* Seção de dados do endereço */}
+                <SectionText 
+                    title="Endereço Residencial" 
+                    titleStyle={{fontSize: 18}}
+                ></SectionText>
+                <AppInput
+                    label={true}
+                    labelText="CEP" 
+                    placeholder="00000-000"
+                ></AppInput>
+                <View style={styles.row}>
+                    <AppInput
+                        label={true}
+                        labelText="Rua/Avenida" 
+                        containerStyle={{flex: 2}}
+                    ></AppInput>
+                    <AppInput
+                        label={true}
+                        labelText="Nº" 
+                        containerStyle={{flex: 1}}
+                    ></AppInput>
+                </View>
+                <AppInput
+                    label={true}
+                    labelText="Bairro" 
+                ></AppInput>
+                <AppInput
+                    label={true}
+                    labelText="Cidade/UF" 
+                ></AppInput>
             </View>
         </View>
     )
