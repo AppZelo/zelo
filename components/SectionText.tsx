02718ca08ce3d4481f3e23e5a9db4fd1f760d148
eiken <117394582+eikqn@users.noreply.github.com>
2026-09-25 16:00:05 -0300

    feat: add `titleStyle` prop to the SectionText component

--- components/SectionText.tsx
+++ components/SectionText.tsx
@@ -1,20 +1,23 @@
 import { View, Text, StyleSheet } from "react-native"
 import { colors } from "../constants/colors"
+import { StyleProp, TextStyle } from "react-native"
 
 type SectionTextProps = {
     title: string,
     showDescription?: boolean,
     description?: string,
+    titleStyle?: StyleProp<TextStyle>
 }
 
 export default function SectionText({
     title,
     showDescription = false,
     description,
+    titleStyle,
 }: SectionTextProps) {
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>{title}</Text>
+            <Text style={[styles.title, titleStyle]}>{title}</Text>
             {showDescription && <Text style={styles.description}>{description}</Text>}
         </View>
     )
